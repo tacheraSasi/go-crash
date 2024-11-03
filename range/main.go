95c428f0e11b0ffd4fe659633df81777bb105550
tacheraSasi <tacherasasi@gmail.com>
2024-11-03 15:35:00 +0300

    prettified it

--- range/main.go
+++ range/main.go
@@ -3,14 +3,15 @@ package main
 import "fmt"
 
 func main(){
-	var names = []string{"Irene","Rachel","Agatha","Stella"}
+	var names []string = []string{"Irene","Rachel","Agatha","Stella"}
 
 	//The normal old fashion for loop
 	for i := 0; i < len(names); i++{
 		fmt.Println("This girl broke my heart",names[i])
 	}
 
-	fmt.Println("\nUsing the range keyword instead\n")
+	fmt.Println("\nUsing the range keyword instead")
+	fmt.Print("-------------------------------\n")
 
 	for i,name := range names{
 		fmt.Println(i+1,"Thiis girl also broke my heart",name)
