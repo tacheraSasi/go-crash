b33d3404a8e1943e5abe5e16b097f9081f922d87
tacheraSasi <tacherasasi@gmail.com>
2024-11-03 15:23:49 +0300

    normal for loop

--- range/main.go
+++ range/main.go
@@ -1,5 +1,12 @@
 package main
 
+import "fmt"
+
 func main(){
-	var names = []string{"Irene"}
+	var names = []string{"Irene","Rachel","Agatha","Stella"}
+
+	//The normal old fashion for loop
+	for i := 0; i < len(names); i++{
+		fmt.Println("This girl broke my heart",names[i])
+	}
 }
