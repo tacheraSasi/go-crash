34d40bc683676a7249e94ae797606eab1d0ec6c5
tacheraSasi <tacherasasi@gmail.com>
2024-11-03 22:23:43 +0300

    function that returns a function

--- functions/main.go
+++ functions/main.go
@@ -2,9 +2,21 @@ package main
 
 import "fmt"
 
+// a function that returns a function
+func returnFunc(x string) func(){
+	return func ()  {
+		fmt.Println(x)
+	}
+}
+
 func main(){
 	inLineFunc := func(){
 		fmt.Println("This is an in line func similar to arrow function in javascript")
 	}
 	inLineFunc()
+
+	// Calling the function that returns a function
+	returnFunc("This function returns a function")() //immediately revoking it 
+
+	
 }
