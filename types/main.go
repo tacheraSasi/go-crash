cb24346ad0ef520bd91b9ffb8b53c27344eb3030
tacheraSasi <tacherasasi@gmail.com>
2024-11-04 23:56:31 +0300

    struct methods

--- types/main.go
+++ types/main.go
@@ -12,6 +12,12 @@ func changeX(point *Point, xVal int32){
 	point.x = xVal//Changing the value of point.x through the actual pointer
 }
 
+//struct method that does not change 
+//Anythin from the actual point
+func (p Point){
+
+}
+
 func main(){
 	p1 := &Point{y:3}//only giving the value to one item in the struct
 	//Leaving the other one to take the default value of its type
