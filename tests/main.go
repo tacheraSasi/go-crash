53ddf41924bb3486b931dfba8fd098191bf79584
Tachera Sasi <tacherasasi@gmail.com>
2025-03-25 13:24:54 +0300

    enhance main.go to support command line arguments for viewing, adding, and removing todos

--- tests/main.go
+++ tests/main.go
@@ -1,7 +1,33 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"tests/todo"
+)
+
+func main() {
+	//using the flag module to parse command line arguments
+	viewFlag := flag.Bool("view", false, "View all todos")
+	addFlag := flag.String("add", "", "Add a new task")
+	removeFlag := flag.Int("remove", -1, "Remove a task by index")
 
 
-func main(){
+	if *viewFlag {
+		todo.AllTodos()
+		return
+	}
 
+	if *addFlag != "" {
+		todo.AddTodo(*addFlag)
+		return
+	}
+
+	if *removeFlag != -1 {
+		todo.RemoveTodo(*removeFlag)
+		return
+	}
+
+	fmt.Println("Usage:")
+	flag.PrintDefaults()
 }
