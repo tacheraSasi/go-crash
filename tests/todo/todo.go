6bdbf27dc6f5c7baf9421dc9177259ac93c9dbc0
Tachera Sasi <tacherasasi@gmail.com>
2025-03-25 13:22:06 +0300

    update todo functions to accept parameters and provide feedback on actions

--- tests/todo/todo.go
+++ tests/todo/todo.go
@@ -1,16 +1,26 @@
 package todo
 
+import "slices"
+
+import "fmt"
+
 var TodoList []string
 
 
-func AddTodo(){
-
+func AddTodo(todo string){
+	TodoList = append(TodoList, todo)
+	fmt.Println("Todo added successfully")
 }
 
-func RemoveTodo(){
+func RemoveTodo(id int){
+	TodoList = slices.Delete(TodoList, id-1, id)
+	fmt.Println("Todo removed successfully")
 
 }
 
 func AllTodos(){
-	
+	for i, todo := range TodoList{
+		fmt.Printf("%d. %s\n", i+1, todo)
+	}
+
 }
